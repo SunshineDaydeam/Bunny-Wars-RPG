76a325beb6e071e71628a1c948d20be3cc86318f
Sunshine Daydream <alexbruner@live.com>
2018-04-27 00:49:11 -0500

    cleaned up javascript and am working on adding more mini features

--- assets/javascript2.js
+++ assets/javascript2.js
@@ -18,146 +18,179 @@ var killCount = 0;              //number of enemies killed
 
 //define global functions
 
-function reset(){             //restart function
-    console.log("NEW GAME," + " wins: " + wins + ", losses: " + losses);
-    userChar = "";
-    enemies = [];
-    currEnemy=[];
-    killCount=0;
-    turnCounter=0;
-    chars.bunny.hp=120;
-    chars.bigBunny.hp = 140;
-    chars.easterBunny.hp = 160;
-    chars.deathBunny.hp = 200;
-    $("#charSelectWindow").show();
-    $("#enemySelectWindow").hide();
-    $("#battleWindow").hide();
+//reset function
+function reset(){                     
+    userChar = "";                              //reset user character
+    enemies = [];                               //reset enemies array
+    currEnemy=[];                               //reset current enemies
+    killCount=0;                                //reset kill counter
+    turnCounter=0;                              //reset turn counter
+    chars.bunny.hp=120;                         //reset bunny health
+    chars.bigBunny.hp = 140;                    //reset big bunny health
+    chars.easterBunny.hp = 160;                 //reset easter bunny health
+    chars.deathBunny.hp = 200;                  //reset death bunny health
 
-    $(".wins").text("Wins: " + wins);
-    $(".losses").text("Losses: " + losses);
+    $("#charSelectWindow").show();              //show character selection screen
+    $("#enemySelectWindow").hide();                 //hide enemy selection screen  
+    $("#battleWindow").hide();                      //hide battle screen
+    $("#winWindow").hide();                         //hide win screen
+    $("#lossWindow").hide();                        //hide loss screen
+    $(".wins").text("Wins: " + wins);           //update wins on navbar
+    $(".losses").text("Losses: " + losses);     //update losses on navbar
 }
-reset();
 
-//////////////////////////select your userBunny///////////////////////////////////////
+reset();                                        //run reset on startup
 
-//user character select function
+
+//////////////////////////////////////////////////////////////////////////////////////
+////////////////////////  SELECT YOUR USER BUNNY  ////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+
+    
+//display character select images
+$("#userBunny").html(chars.bunny.image);
+$("#userBigBunny").html(chars.bigBunny.image);
+$("#userEasterBunny").html(chars.easterBunny.image);
+$("#userDeathBunny").html(chars.deathBunny.image);
 
 $('#userBunny').click (function(){          //when bunny is selected
-    userChar = chars.bunny;
-    enemies.push(chars.bigBunny);
-    enemies.push(chars.easterBunny);
-    enemies.push(chars.deathBunny);
-    charSelect();
+    userChar = chars.bunny;                 //define bunny as user character
+    enemies.push(chars.bigBunny);           //push other characters to enemies array
+    enemies.push(chars.easterBunny);        //
+    enemies.push(chars.deathBunny);         //
+    charSelect();                           //run charSelect Function
 });
 
 $('#userBigBunny').click (function(){       //when bigBunny is selected
-    userChar = chars.bigBunny;
-    enemies.push(chars.bunny);
-    enemies.push(chars.easterBunny);
-    enemies.push(chars.deathBunny);
-    charSelect();
+    userChar = chars.bigBunny;              //define big bunny as user character
+    enemies.push(chars.bunny);              //push other characters to enemies array
+    enemies.push(chars.easterBunny);        //
+    enemies.push(chars.deathBunny);         //
+    charSelect();                           //run charSelect Function
 });
 
 $('#userEasterBunny').click (function(){    //when easterBunny is selected
-    userChar = chars.easterBunny;
-    enemies.push(chars.bunny);
-    enemies.push(chars.bigBunny);
-    enemies.push(chars.deathBunny);
-    charSelect();
+    userChar = chars.easterBunny;           //define easter bunny as user character
+    enemies.push(chars.bunny);              //push other characters to enemies array
+    enemies.push(chars.bigBunny);           //
+    enemies.push(chars.deathBunny);         //
+    charSelect();                           //run charSelect Function
 });
 
 $('#userDeathBunny').click (function(){     //when deathBunny is selected
-    userChar = chars.deathBunny;
-    enemies.push(chars.bunny);
-    enemies.push(chars.bigBunny);
-    enemies.push(chars.easterBunny);
-    charSelect();
+    userChar = chars.deathBunny;            //define death bunny as user character
+    enemies.push(chars.bunny);              //push other characters to enemies array
+    enemies.push(chars.bigBunny);           //
+    enemies.push(chars.easterBunny);        //
+    charSelect();                           //run charSelect Function
 });
 
+//character select function
+//runs when your user character is selected
 function charSelect(){              
-    $("#enemy1").html(enemies[0].image);
-    $("#enemy2").html(enemies[1].image);
-    $("#enemy3").html(enemies[2].image);
-    $("#userCharNavName").html(userChar.image)
+    $("#enemy1").html(enemies[0].image);            //display character images to enemies Select Window
+    $("#enemy2").html(enemies[1].image);            //
+    $("#enemy3").html(enemies[2].image);            //
+    $("#userCharNavName").html(userChar.image)      //display user character image on navbar
+    $(".health").text("HP: " + userChar.hp);
+    $(".attack").text("Attack Power: " + userChar.attack);
+    
     $("#charSelectWindow").hide();
     $("#enemySelectWindow").show();
+    
     console.log("you are the " + userChar.name + " your hp: " + " " + userChar.hp);
 }
 
-    //display character select icons
-    $("#userBunny").html(chars.bunny.image);
-    $("#userBigBunny").html(chars.bigBunny.image);
-    $("#userEasterBunny").html(chars.easterBunny.image);
-    $("#userDeathBunny").html(chars.deathBunny.image);
 
-//////////////////////////////////// Select Enemy Bunny ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+////////////////////////     SELECT YOUR ENEMY    ////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
 
 
-//enemy select function   
-
-function updateEnemyList(){
-    $("#enemySelectWindow").hide();
-    $("#battleWindow").show(); 
-    $("#userFightIcon").html (userChar.image);
-    $("#enemyFightIcon").html(currEnemy.image);
-        if (enemies[0] != undefined){
-            $("#enemy1").html(enemies[0].image);}
-        if (enemies[0] == undefined){
-            $("#enemy1").html("");}
-        if (enemies[1] != undefined){
-            $("#enemy2").html(enemies[1].image);}
-        if (enemies[1] == undefined){
-            $("#enemy2").html("");}
-        if (enemies[2] != undefined){
-            $("#enemy3").html(enemies[2].image);}
-        if (enemies[2] == undefined){
-            $("#enemy3").html("");}
-
+//update Enemies Function 
+function updateEnemyList(){                         
+    $("#enemySelectWindow").hide();                 //hide enemy selection window
+    $("#battleWindow").show();                      //show battle window
+    $("#userFightIcon").html (userChar.image);      //display user bunny image on battlescreen
+    $("#enemyFightIcon").html(currEnemy.image);     //display enemy bunny image on battlescreen
+        if (enemies[0] != undefined){               //if 1 enemy exists
+            $("#enemy1").html(enemies[0].image);}       //display its image 
+        if (enemies[0] == undefined){               //if not
+            $("#enemy1").html("");}                     //display nothing in its spot
+        if (enemies[1] != undefined){               //if 2nd enemy exists
+            $("#enemy2").html(enemies[1].image);}       //display it's image
+        if (enemies[1] == undefined){               //if not
+            $("#enemy2").html("");}                     //display nothing in its spot
+        if (enemies[2] != undefined){               //if 3rd enemy exists
+            $("#enemy3").html(enemies[2].image);}       //display it's image
+        if (enemies[2] == undefined){               //if not
+            $("#enemy3").html("");}                     //display nothing in its spot
 }
 
-$("#enemy1").click (function(){
-    currEnemy = (enemies[0]);
-    enemies.splice (0,1);
-    console.log(currEnemy);
-    updateEnemyList();
-});
-$("#enemy2").click (function(){
-    currEnemy = enemies[1];
-    enemies.splice (1,1);
-    updateEnemyList();
+//Enemy1 Click
+$("#enemy1").click (function(){             //if enemy 1 is selected
+    currEnemy = (enemies[0]);               //define selected enemy ast currEnemy
+    enemies.splice (0,1);                   //remove selected enemy from enemies array                     
+    updateEnemyList();                      //run update enemies function
     console.log(currEnemy);
 });
-$("#enemy3").click (function(){
-    currEnemy = enemies[2];
-    enemies.splice (2,1);
-    updateEnemyList();
-    console.log(currEnemy);
+$("#enemy2").click (function(){             //if enemy 1 is selected
+    currEnemy = enemies[1];                 //define selected enemy ast currEnemy
+    enemies.splice (1,1);                   //remove selected enemy from enemies array
+    updateEnemyList();                      //run update enemies function
+    console.log(currEnemy);                 
+});
+$("#enemy3").click (function(){             //if enemy 1 is selected
+    currEnemy = enemies[2];                 //define selected enemy ast currEnemy
+    enemies.splice (2,1);                   //remove selected enemy from enemies array
+    updateEnemyList();                      //run update enemies function
+    console.log(currEnemy);                 
 });
 
 
-//attack
+//////////////////////////////////////////////////////////////////////////////
+////////////////////////                              ////////////////////////
+////////////////////////     BUNNY BATTLE SHOWDOWN    ////////////////////////
+////////////////////////                              ////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+//When attack Button Clicked
 $("#attack").click (function(){
-if (currEnemy.hp >= 0){
-    currEnemy.hp = currEnemy.hp - (userChar.attack * turnCounter);
+if (currEnemy.hp > 0){                                              //if enemy's health is greater than 0
+    currEnemy.hp = currEnemy.hp - (userChar.attack * turnCounter);  //reduce enemies health by user attack times the number of turns
     $("#attackText").text("you attacked for " + userChar.attack * turnCounter + " enemies hp: " + currEnemy.hp);
-    turnCounter++;    
-    if (currEnemy.hp <= 0){
-        killCount++;
-        console.log ("You Killed the " + currEnemy + " enemy HP: " + userChar.hp);
-        $("#enemySelectWindow").show();
-        $("#battleWindow").hide();
-        if (killCount == 3){
-            wins++;
-            reset();
+    turnCounter++;                                                  //increase turn counter + 1
+    
+    //If enemy dies
+    if (currEnemy.hp <= 0){                                             
+        killCount++;                                                //increase killcount +1
+        $("#enemySelectWindow").show();                             //show enemy select window
+        $("#battleWindow").hide();                                  //hide battle window
+        
+        //if you killed all 3 bunnies
+        if (killCount == 3){                        
+            wins++;                                                 //increase wins +1
+            $("#enemySelectWindow").hide();                         //hide enemy select window
+            $("#winWindow").show();                                 //show Congratulations window
+            $(".reset").click (function(){                          //click reset button to go play again
+                reset();
+            })
         }
     }
+    //If enemy survives the attack
     else {
-        userChar.hp -= currEnemy.counterAttack;
+        userChar.hp -= currEnemy.counterAttack;                     //user hp is reduced by current enemies counter-attack
         $("#counterAttackText").text(currEnemy.name + "countered for " + currEnemy.counterAttack + " your hp: " + userChar.hp);
-        if (userChar.hp <= 0){
-            losses++;
-            console.log("The " + currEnemy.name +" killed you ");
-            reset();
+        
+        //If user's bunny dies :(
+        if (userChar.hp <= 0){                                      
+            losses++;                                               //increase losses by 1
+            console.log("The " + currEnemy.name +" killed you ");   
+            $("#battleWindow").hide();                              //hide battle window
+            $("#lossWindow").show();                                //show the You Lost window
+            $(".reset").click (function(){                          //click reset button to play again
+                reset();
+            })
         }   
     }
 }
