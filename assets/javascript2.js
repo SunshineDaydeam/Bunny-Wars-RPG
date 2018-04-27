d5622f3d63be55ba924db206604932c68a405d66
Sunshine Daydream <alexbruner@live.com>
2018-04-27 02:35:33 -0500

    added lots of little things...

--- assets/javascript2.js
+++ assets/javascript2.js
@@ -1,6 +1,6 @@
 //define characters
 let chars = {
-    'bunny' : {name: 'Regular Sized Bunny',hp: 120,attack: 8,counterAttack: 12, image: "<img src='assets/images/bunnySmall.jpg'>"},
+    'bunny' : {name: 'Regular Sized Bunny',hp: 250,attack: 8,counterAttack: 12, image: "<img src='assets/images/bunnySmall.jpg'>"},
     'bigBunny' : {name: 'Big Bad Bunny',hp: 140,attack: 12,counterAttack: 25, image: "<img src='assets/images/bunnyBig.jpeg'>"},
     'easterBunny' : {name: 'The Easter Bunny',hp: 160,attack: 15,counterAttack: 15, image: "<img src='assets/images/bunnyEaster.jpg'>"},
     'deathBunny' : {name: 'The Death Bunny',hp: 200,attack: 20,counterAttack: 20, image: "<img src='assets/images/bunnyEvil.jpg'>"}
@@ -24,22 +24,40 @@ function reset(){
     enemies = [];                               //reset enemies array
     currEnemy=[];                               //reset current enemies
     killCount=0;                                //reset kill counter
-    turnCounter=0;                              //reset turn counter
-    chars.bunny.hp=120;                         //reset bunny health
+    turnCounter=1;                              //reset turn counter
+    chars.bunny.hp=250;                         //reset bunny health
     chars.bigBunny.hp = 140;                    //reset big bunny health
     chars.easterBunny.hp = 160;                 //reset easter bunny health
     chars.deathBunny.hp = 200;                  //reset death bunny health
 
+    $("#userStatsBar").show();                  //show user stats bar
     $("#charSelectWindow").show();              //show character selection screen
+    $("#startGameWindow").hide();               //hide "how to play" window
     $("#enemySelectWindow").hide();                 //hide enemy selection screen  
     $("#battleWindow").hide();                      //hide battle screen
     $("#winWindow").hide();                         //hide win screen
     $("#lossWindow").hide();                        //hide loss screen
     $(".wins").text("Wins: " + wins);           //update wins on navbar
     $(".losses").text("Losses: " + losses);     //update losses on navbar
+    $("#counterAttackText").text("");
+    $("#attackText").text("");
+
+    $("#userCharNavName").html("")      //display user character image on navbar
+    $(".health").text("HP: " + 0);
+    $(".attack").text("Attack Power: " + 0);
 }
 
-reset();                                        //run reset on startup
+$("#userStatsBar").hide();
+$("#charSelectWindow").hide();              //show character selection screen
+$("#startGameWindow").show();
+$("#enemySelectWindow").hide();                 //hide enemy selection screen  
+$("#battleWindow").hide();                      //hide battle screen
+$("#winWindow").hide();                         //hide win screen
+$("#lossWindow").hide();                        //hide loss screen
+
+$("#startGame").click(function(){       //run reset function when User clicks start Game
+    reset();  
+})
 
 
 //////////////////////////////////////////////////////////////////////////////////////
@@ -111,8 +129,11 @@ function charSelect(){
 function updateEnemyList(){                         
     $("#enemySelectWindow").hide();                 //hide enemy selection window
     $("#battleWindow").show();                      //show battle window
-    $("#userFightIcon").html (userChar.image);      //display user bunny image on battlescreen
-    $("#enemyFightIcon").html(currEnemy.image);     //display enemy bunny image on battlescreen
+    $("#userFightIcon").html (userChar.image + userChar.hp);      //display user bunny image on battlescreen
+    $("#enemyFightIcon").html(currEnemy.image + currEnemy.hp);     //display enemy bunny image on battlescreen
+    $("#counterAttackText").text("");               //Delete the CounterAttack Text from BattleScreen
+    $("#attackText").text("");                      //Delete the Attack Text from BattleScreen
+    
         if (enemies[0] != undefined){               //if 1 enemy exists
             $("#enemy1").html(enemies[0].image);}       //display its image 
         if (enemies[0] == undefined){               //if not
@@ -154,12 +175,25 @@ $("#enemy3").click (function(){             //if enemy 1 is selected
 ////////////////////////                              ////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 
+
+//update battleDisplay Function
+function updateBattleStats(){
+    //battle window
+    $("#userFightIcon").html (userChar.image + " " + userChar.hp);      
+    $("#enemyFightIcon").html(currEnemy.image + " " + currEnemy.hp);
+    //navbar stats
+    $("#userCharNavName").html(userChar.image)  
+    $(".health").text("HP: " + userChar.hp);
+    $(".attack").text("Attack Power: " + userChar.attack * turnCounter);
+}
+
 //When attack Button Clicked
 $("#attack").click (function(){
 if (currEnemy.hp > 0){                                              //if enemy's health is greater than 0
     currEnemy.hp = currEnemy.hp - (userChar.attack * turnCounter);  //reduce enemies health by user attack times the number of turns
-    $("#attackText").text("you attacked for " + userChar.attack * turnCounter + " enemies hp: " + currEnemy.hp);
-    turnCounter++;                                                  //increase turn counter + 1
+    $("#attackText").text("You Attacked For " + userChar.attack * turnCounter + " Damage!");
+    turnCounter++;
+    updateBattleStats();                                                  //increase turn counter + 1
     
     //If enemy dies
     if (currEnemy.hp <= 0){                                             
@@ -180,8 +214,9 @@ if (currEnemy.hp > 0){                                              //if enemy's
     //If enemy survives the attack
     else {
         userChar.hp -= currEnemy.counterAttack;                     //user hp is reduced by current enemies counter-attack
-        $("#counterAttackText").text(currEnemy.name + "countered for " + currEnemy.counterAttack + " your hp: " + userChar.hp);
-        
+        $("#counterAttackText").text(currEnemy.name + " countered for " + currEnemy.counterAttack + " Damage!");
+        updateBattleStats()
+
         //If user's bunny dies :(
         if (userChar.hp <= 0){                                      
             losses++;                                               //increase losses by 1
